f1c0dbafd1f1a412c326b49b958fa35272db7ce4
Peter Cruckshank <petecruckshank@gmail.com>
2018-03-06 15:58:19 -0500

    made the reverseInt() function work and account for sign

--- session1/index.js
+++ session1/index.js
@@ -8,55 +8,58 @@ function reverseString(str) {
     // Solution by Peter Cruckshank @Github - PeterCr
     let revString = str.split('').reverse().join('');
     return revString;
-    
+
   }
-  
-  
-  
+
+
+
   // CHALLENGE 2: VALIDATE A PALINDROME
   // Return true if palindrome and false if not
   // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
-  
+
   function isPalindrome(str) {
       // I thought it would be fun to just invoke the function we just made
-      // originally used a if/then statement, but it's unnecessary 
+      // originally used a if/then statement, but it's unnecessary
      let revStr = reverseString(str);
 
      return str === revStr;
   }
-  
-  
-  
+
+
+
   // CHALLENGE 3: REVERSE AN INTEGER
   // Return an integer in reverse
   // ex. reverseInt(521) === 125
-  
-  function reverseInt(int) {}
-  
-  
-  
+
+  function reverseInt(int) {
+    let revString = int.toString().split('').reverse().join('');
+
+    return parseInt(revString) * Math.sign(int);
+  }
+
+
+
   // CHALLENGE 4: CAPITALIZE LETTERS
   // Return a string with the first letter of every word capitalized
   // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
   function capitalizeLetters(str) {}
-  
-  
-  
+
+
+
   // CHALLENGE 5: MAX CHARACTER
   // Return the character that is most common in a string
   // ex. maxCharacter('javascript') == 'a'
   function maxCharacter(str) {}
-  
-  
-  
+
+
+
   // CHALLENGE 6: FIZZBUZZ
   // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
   function fizzBuzz() {}
-  
-  
-  
+
+
+
   // Call Function
-  const output = isPalindrome('racecar');
-  
+  const output = reverseInt(-54321);
+
   console.log(output);
-  
